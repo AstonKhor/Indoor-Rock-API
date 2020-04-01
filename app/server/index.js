954dd7d6aa8cad9a172802b307ad456ad6e6820d
AstonKhor <khoraston@gmail.com>
2020-04-01 15:58:41 -0700

    design endpoints on server

--- app/server/index.js
+++ app/server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const port = 3000;
+const routes = require('./routes/routes.js');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/indoorGyms/:region', (req, res) => {
-  console.log('req recieved');
-})
+app.get('/indoorGyms/api/json', routes.getGyms);
+
+app.post('/user', routes.postUser);
 
 app.listen(port, () => { console.log(`Now listening on port ${port}`)})
