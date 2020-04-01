03119f993306e8ce988a6957e7fb22cc73bbb7a2
AstonKhor <khoraston@gmail.com>
2020-03-31 22:00:43 -0700

    create webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,23 +1,26 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'client/dist'),
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
 };
