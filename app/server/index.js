db2317fff6adf0d8ef5db4ad25d0a1245ec2426b
AstonKhor <khoraston@gmail.com>
2020-03-31 22:01:35 -0700

    set up express server

--- app/server/index.js
+++ app/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const port = 3000;
 
+const app = express();
 
-app.search(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/indoorGyms/:region', (req, res) => {
   console.log('req recieved');
 })
+
+app.listen(port, () => { console.log(`Now listening on port ${port}`)})
