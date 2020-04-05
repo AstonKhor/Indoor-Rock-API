f30a012a3fb38962f8a9174fbbb52d4fbecce9ad
AstonKhor <khoraston@gmail.com>
2020-04-05 11:53:31 -0700

    Add pagination to results

--- app/client/src/components/Results.jsx
+++ app/client/src/components/Results.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import ResultCard from './ResultCard';
+import Pagination from '@material-ui/lab/Pagination';
 import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
-  }
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
 }));
 
 export default function Results({ gyms, params}) {
   const classes = useStyles();
+  const [page, setPage] = React.useState(1);
   let resultGyms = [];
   if (params.length === 0) {
     resultGyms = gyms;
@@ -25,13 +29,23 @@ export default function Results({ gyms, params}) {
       }
     }
   }
+  let pageGyms = [];
+  for (let i = 0; i < resultGyms.length; i++) {
+    if (i < page * 10 && i >= (page-1) * 10) {
+      pageGyms.push(resultGyms[i]);
+    }
+  }
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
   return (
     <Grid container justify="center" spacing={2}>
-      {resultGyms.map((gym) => (
+      <Pagination count={Math.ceil(resultGyms.length / 10)} page={page} onChange={handlePageChange} />
+      {pageGyms.map((gym) => (
         <Grid key={`${gym.Id}${gym.Country}`} item>
-          <LazyLoadComponent>
-            <ResultCard gym={gym}></ResultCard>
-          </LazyLoadComponent>
+          <ResultCard gym={gym}></ResultCard>
         </Grid>
       ))}
     </Grid>
