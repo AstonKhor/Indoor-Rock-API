41f03e9289df4dbfbb0794696656f3c0abc05ec5
AstonKhor <khoraston@gmail.com>
2020-04-07 13:07:06 -0700

    createpool db connection

--- app/database/index.js
+++ app/database/index.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
 if (process.env.HEROKU) {
-  connection = mysql.createConnection({
+  connection = mysql.createPool({
     host     : 'us-cdbr-iron-east-01.cleardb.net',
     user     : 'b12b001a219e57',
     password : '077891b6',
     database : 'heroku_a47c644aa249fa2'
   });
 } else {
-  connection = mysql.createConnection({
+  connection = mysql.createPool({
     host     : 'localhost',
     user     : 'root',
     database : 'indoorGyms'
