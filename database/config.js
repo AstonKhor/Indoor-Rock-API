2dac75f5475ea1a8be4059eed4394fc0383f944e
AstonKhor <khoraston@gmail.com>
2020-04-07 18:40:07 -0700

    upate config db

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host     : process.env.HEROKU ? 'us-cdbr-iron-east-01.cleardb.net' : 'localhost',
+  host     : process.env.HEROKU ? 'ec2-52-200-119-0.compute-1.amazonaws.com' : 'localhost',
   user     : process.env.HEROKU ? 'xytqujyhjetdns' : 'AstonKhor',
   password : process.env.HEROKU ? '077891b6' : '',
   database : process.env.HEROKU ? 'd1o31vt6s14bc2' : 'indoorgyms'
