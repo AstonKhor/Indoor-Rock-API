ac29a452254536a8e6dc87c24efc58df707428ae
AstonKhor <khoraston@gmail.com>
2020-04-07 19:08:33 -0700

    changes to fix heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const routes = require('./routes/routes.js.js');
+const routes = require('./routes/routes.js');
 let port = process.env.PORT || 3000;
 
 const app = express();
